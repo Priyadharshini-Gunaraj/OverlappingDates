7b1901eec7cbaefc13096847a070db905547cb1f
Priya Dharshini <priyadharshini@Priyas-MacBook-Air.local>
2023-03-29 15:33:45 -0700

    add new router and home page

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/date-range/overlap", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("api hit")
+	}).Methods("GET")
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module overlappingdates
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
